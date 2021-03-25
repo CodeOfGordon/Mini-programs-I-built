d7e8a3bdd9adb5a98f2935c502a20e81c151ae97
CodingNoobGordon <81336844+CodingNoobGordon@users.noreply.github.com>
2021-03-25 18:40:06 -0400

    I added the whole thing lol
    This program calculates the 2 square roots off a standard form equation:
    (By standard form I mean ax^2 + bx + c)

---
+++ The roots calculator.py
@@ -0,0 +1,33 @@
+#Initialize variables
+import math
+end = False
+
+#Process - Function calculates the quadratic formula
+def roots(a,b,c):
+    #Quadratic formula, inside square root part: b^2 - 4ac
+    firstPart = b**2 - 4*a*c
+    
+    #Quadratic formula, the rest part, positive: [-b + sqrt(b^2 - 4ac)] / 2a
+    positiveSqrt = (-b + math.sqrt(firstPart)) / (2*a)
+    #Quadratic formula, the rest part, negative: [-b - sqrt(b^2 - 4ac)] / 2a
+    negativeSqrt = (-b - math.sqrt(firstPart)) / (2*a)
+    return positiveSqrt, negativeSqrt
+
+#Input & Output - Main program outputs the roots
+print("This program will calculate the roots from the quadratic formula \n\
+*Should you insist on exiting this exquisite program, please type 'booby'*")
+while end == False:
+    try:
+        #Receives variables and prints the roots.
+        a = float(input("Enter the 'a' variable: "))
+        b = float(input("Enter the 'b' variable: "))
+        c = float(input("Enter the 'c' variable: "))
+        posSqrt, negSqrt = roots(a,b,c)
+        print(f"The roots are {posSqrt:.2f} and {negSqrt:.2f}")
+        end = True
+          
+    except:
+        print("*"*40)
+        print("The roots don't exist for this combination!\n\
+Either that or you typed in the wrong thing...")
+        end == False
