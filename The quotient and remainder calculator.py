2062a54c0171eafec7de5079858447524dce6e46
CodingNoobGordon <81336844+CodingNoobGordon@users.noreply.github.com>
2021-03-25 19:21:11 -0400

    Removal
    Removes the school test cases

--- The quotient and remainder calculator.py
+++ The quotient and remainder calculator.py
@@ -1,15 +1,3 @@
-# Name: Gordon
-# Date: March 24, 2021
-# File Name: d2q2_quotientRemainder.py
-# Description: Receives and prints the user's full name
-# Test cases: Input             Expected Output                     Pass/Fail
-#               7,2                 3 and 1                         Pass
-#               18,5                3 and 3                         Pass
-#               72,12               6 and 0                         Pass
-#                0,12               0 and 0                         Pass
-#                3,8                0 and 3                         Pass
-#                3,0                error                           Pass
-
 #Process - Function calculates and returns the quotient and remainder
 def quot_rem(num1, num2):
     quotient = num1 // num2
